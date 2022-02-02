dac3cbc6df0a297732125872612b6db9b5bd6d55
Heli Eronen <herojonen@gmail.com>
2022-02-02 18:24:14 +0200

    Osa2 tehtävät 2.1-2.5

---
+++ Osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,50 @@
+const Header = ({ name }) => {
+    return (
+      <div>
+        <h2>{name}</h2>
+      </div>
+    )
+  }
+  
+  const Part = ({ part }) => {
+    return (
+      <div>
+        <p>{part.name} {part.exercises}</p>
+      </div>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map((part, i) => 
+          <Part part={part} key={i} />
+        )}
+      </div>
+    )
+  }
+  
+  const Total = ({ parts }) => {
+    const sum = parts.reduce((initial, part2) => {
+        return (initial + part2.exercises)
+    }, 0)
+    return (
+      <div>
+        <p><strong>total of {sum} exercises</strong></p>
+      </div>
+    )
+    
+    
+  }
+  
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    )
+  }
+
+  export default Course
